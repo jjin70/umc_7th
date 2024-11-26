9ccf102820cec22bb0129629288b16556935bb4c
jjin70 <jjin0434@naver.com>
2024-11-26 10:21:19 +0900

    feat: 가게의 리뷰 목록 조회 구현 #14

--- src/main/java/umc/spring/service/StoreService/StoreQueryService.java
+++ src/main/java/umc/spring/service/StoreService/StoreQueryService.java
@@ -1,6 +1,8 @@
 package umc.spring.service.StoreService;
 
+import org.springframework.data.domain.Page;
 import umc.spring.domain.Store;
+import umc.spring.domain.Review;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,4 +11,5 @@ public interface StoreQueryService {
 
     Optional<Store> findStore(Long id);
     List<Store> findStoresByNameAndScore(String name, Float score);
+    Page<Review> getReviewList(Long StoreId, Integer page);
 }
