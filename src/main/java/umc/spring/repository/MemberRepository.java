58acb410fa134c9688a72668e7ae4fd6cb0a8694
jjin70 <jjin0434@naver.com>
2024-12-18 19:52:37 +0900

    feat: 로그인 구현 #25

--- src/main/java/umc/spring/repository/MemberRepository.java
+++ src/main/java/umc/spring/repository/MemberRepository.java
@@ -1,5 +1,6 @@
 package umc.spring.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,4 +14,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
 //    @Query("SELECT m FROM Member m WHERE m.name = :name AND m.status = :status")
 //    List<Member> findByNameAndStatus(@Param("name") String name, @Param("status") MemberStatus status);
+
+    Optional<Member> findByEmail(String email);
 }
